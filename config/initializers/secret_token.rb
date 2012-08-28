c292e47a6ada7e1d8453432da75bc97f039b23d2
saikiranmothe <saikiran.mothe@gmail.com>
2012-08-28 11:26:10 +0530

    Html and css added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapsmaps::Application.config.secret_token = 'f393d376ee25c884e0496f39286110584e454ba21a3c97c824df96312060cce2c3ad77247f3c83e54db1f15e2afd33c7a6e74d73937df66fe08534ea7b1f4ca1'
